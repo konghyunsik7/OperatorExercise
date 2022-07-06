5f0cc90da6c87c9c16643802b60750a4571cd76c
Hyun Kong <konghyunsik7@gmail.com>
2022-07-06 15:08:35 -0400

    Exercise 1 & 2

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using OperatorExcerise;
+
+//Excerise1
+
+int a = 17;
+int b = 4;
+int quotient = a / b;
+int remainder = a % b;
+
+Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+
+
+//Exercise2
+
+
+Console.WriteLine("What is the radius of your circle?");
+
+var stansVariable = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"The area of a circle with radius of {stansVariable} is {Methods.AreaOfCircle(stansVariable)}");
+
+
+
+
+
+
+
+
